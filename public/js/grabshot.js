1dc099e3f3ffa336fcc9c5f724ee6ec29e4eb04e
Bo Jeanes <me@bjeanes.com>
2013-04-16 13:49:44 -0500

    Add placeholder image on form submit

--- public/js/grabshot.js
+++ public/js/grabshot.js
@@ -11,19 +11,25 @@
       $('#demo .alert.cors').hide();
 
       form.on('submit', function(event) {
+        var img = $('<img>').attr({
+          'data-src': 'holder.js/205x154/text:Fetching...'
+        }).appendTo($('#demo .output'));
+
+        Holder.run();
+
         $.post(rbApi, function(postbin) {
            var url = 'http://requestb.in/' + postbin.name;
            var link = '<a href="' + url + '?inspect">' + url + '</a>';
            var content = 'Using ' + link + ' as callback URL';
            form.find('small.note').html(content);
-           grabshot(postbin);
+           grabshot(postbin, img);
          });
 
         return event.preventDefault();
       });
     }
 
-    grabshot = function(postbin) {
+    grabshot = function(postbin, img) {
       var binUrl = rbApi + '/' + postbin.name;
 
       var params = {
@@ -38,16 +44,12 @@
           $.get(binUrl, function(data) {
             if(data["request_count"] > 0) {
               $.get(binUrl + '/requests', function(data) {
-                var data = $.parseJSON(data[0].body),
-                attrs = {
+                var data = $.parseJSON(data[0].body);
+                img.attr({
                   src: 'data:image/png;base64,' + data.imageData,
-                  alt: data.title,
-                  width: 205,
-                  height: 154
-                },
-                img = $('<img>').attr(attrs);
-
-                $('#demo').append(img);
+                  'data-src': null,
+                  alt: data.title
+                });
               });
             } else {
               window.setTimeout(fn, 100);
