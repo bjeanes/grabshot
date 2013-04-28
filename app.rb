b03846b9f67313782505957fedc329424420ae4c
Bo Jeanes <me@bjeanes.com>
2013-04-28 14:33:31 -0500

    Move screenshotter class to own file

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ Bundler.setup(:default, ENV["RACK_ENV"] || :development)
 require 'sinatra'
 require 'slim'
 
+lib = File.expand_path("../lib", __FILE__)
+$: << lib unless $:.include?(lib)
+require "screenshotter"
+
 configure :development do
   require 'rack/reloader'
   Sinatra::Application.reset!
@@ -48,129 +52,4 @@ post '/snap' do
     status 400
     "ERROR"
   end
-end
-
-require 'thread'
-
-class Screenshotter
-  QUEUE  = Queue.new
-  SCRIPT = File.expand_path('../render.coffee', __FILE__)
-
-  (ENV['WORKER_COUNT'] || 1).to_i.times do |worker_id|
-    Thread.new do
-      me = Thread.current
-      me[:id] = worker_id
-      me[:job_id] = -1
-
-      loop do
-        begin
-          me[:job_id] += 1
-          Screenshotter.take QUEUE.pop, "#{me[:id]}-#{me[:job_id]}"
-        rescue => e
-          log_exception e
-        end
-      end
-    end
-  end
-
-  def self.take(params, id)
-    puts "[#{id}] Processing: #{params.inspect}"
-    params = params.dup
-
-    url      = params[:url].to_s
-    format   = params[:format].to_s.upcase
-    width    = params[:width]
-    height   = params[:height]
-    cmd      = "phantomjs #{SCRIPT} #{url.inspect} #{format} #{width} #{height}"
-
-    puts "[#{id}] Executing: #{cmd}"
-    json = JSON.parse(%x[#{cmd}])
-
-    params.merge!(
-      width: json["width"],
-      height: json["height"],
-      title: json["title"],
-      imageData: json["imageData"])
-
-    respond(:success, params)
-  rescue JSON::ParserError => e
-    log_exception(e)
-    respond(:error, params)
-  ensure
-    params[:imageData] = params[:imageData].to_s[0, 20] + "..."
-    puts "[#{id}] Processed: #{params.inspect}"
-  end
-
-  def self.plan(params)
-    if valid? params
-      QUEUE.push params
-      true
-    else
-      false
-    end
-  rescue
-    false
-  end
-
-  private
-
-  def self.respond(status, params = {})
-    require 'net/http'
-    require 'net/https'
-
-    params[:status] = status
-
-    uri = params[:callback]
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.ssl_timeout = 5
-    http.open_timeout = 5
-    http.read_timeout = 10
-    http.continue_timeout = 10
-    http.use_ssl = true if params[:callback].scheme == "https"
-    headers = {'Content-Type' =>'application/json'}
-    request = Net::HTTP::Post.new(uri.request_uri, headers)
-    request["User-Agent"] = "Grabshot (https://github.com/bjeanes/grabshot)"
-    request.body = params.to_json
-    http.request(request)
-  rescue => e
-    log_exception e
-  end
-
-  def self.valid?(params)
-    !!(http?(params[:url]) &&
-      http?(params[:callback]) &&
-      params[:width].to_i <= 4000 &&
-      params[:format] =~ /^jpe?g|gif|png$/i)
-  end
-
-  def self.http?(uri)
-    !!(uri &&
-      uri.scheme =~ /^https?$/ &&
-      uri.host &&
-      (development? ||
-        uri.host !~ /^\d|localhost|\[/))
-  end
-
-  def self.development?
-    Sinatra::Application.settings.environment.to_s == "development"
-  end
-
-  if ENV['KEEP_ALIVE_URL']
-    require 'open-uri'
-
-    Thread.new do
-      loop do
-        sleep 300
-
-        unless QUEUE.empty?
-          open(ENV['KEEP_ALIVE_URL'])
-        end
-      end
-    end
-  end
-end
-
-def log_exception(e)
-  STDERR.puts "#{e.class}: #{e.message}"
-  STDERR.puts e.backtrace.join("\n")
 end
